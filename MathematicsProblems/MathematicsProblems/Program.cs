2a52dd527e74ce3eec6e6296159b4630dda8142b
TarasSobchyshak <sobchyshak@gmail.com>
2016-04-26 22:46:10 +0300

    First problem solved.

---
+++ MathematicsProblems/MathematicsProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemsSolutions;
+
+namespace MathematicsProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = { -2, -1, 1, 4 };
+            double[] y = { -3, -1, 2, 3 };
+            FirstProblem fp = new FirstProblem(x, y);
+            Console.WriteLine("First problem output:");
+            Console.WriteLine($"y = {fp.M:0.0000} x + {fp.B:0.0000}");
+            Console.WriteLine($"SErl = {fp.SErl():0.0000}");            // regression line errors 
+            Console.WriteLine($"SEy = {fp.SEy():0.0000}");              // Y mean line errors  
+            Console.WriteLine($"R squared = {fp.Rsquared():0.0000}");
+            Console.WriteLine($"X (7) = {((7 - fp.B) / fp.M):0.0000}");
+            Console.WriteLine($"Y (2.5) = {(2.5 * fp.M + fp.B):0.0000}");
+
+            Console.WriteLine();
+            Console.WriteLine("Second problem output:");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MathematicsProblems/ProblemsSolutions/FirstProblem.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using static System.Math;
+
+namespace ProblemsSolutions
+{
+    public class FirstProblem
+    {
+        public FirstProblem(double[] x, double[] y)
+        {
+            X = x;
+            Y = y;
+            Init();
+        }
+
+        private double x;
+        private double y;
+        private double xy;
+        private double xx;
+        public double M
+        {
+            get;
+            private set;
+        }
+        public double B
+        {
+            get;
+            private set;
+        }
+        public int N
+        {
+            get;
+            private set;
+        }
+        public double[] X
+        {
+            get;
+            private set;
+        }
+        public double[] Y
+        {
+            get;
+            private set;
+        }
+
+        private void Init()
+        {
+            x = Mean(X);
+            y = Mean(Y);
+            xy = Mean(X, Y);
+            xx = Mean(X, X);
+            N = X.Length;
+            M = (x * y - xy) / (x * x - xx);
+            B = y - M * x;
+        }
+
+        public double SErl()
+        {
+            double sum = 0;
+            for (int i = 0; i < X.Length; ++i)
+            {
+                sum += Pow(Y[i] - (M * X[i] + B), 2);
+            }
+            return sum / N / N;
+        }
+        public double SEy()
+        {
+            double sum = 0;
+            for (int i = 0; i < X.Length; ++i)
+            {
+                sum += Pow(Y[i] - y, 2);
+            }
+            return sum / N / N;
+        }
+
+        public double Rsquared() => 1 - SErl() / SEy();
+
+        public static double Mean(double[] a) => a.Sum() / a.Length;
+
+        public static double Mean(double[] a, double[] b)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.Length; ++i)
+            {
+                sum += a[i] * b[i];
+            }
+            return sum / a.Length;
+        }
+    }
+}
